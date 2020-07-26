022aa1b56897f3e95dac6e60b5cd3e6534038090
Alex Rosenfeld <arosenfeld2003@mac.com>
2020-07-26 12:33:00 -0700

    lyft app

---
+++ README.md
@@ -0,0 +1,11 @@
+This app requires the Sinatra gem.
+  * gem install sinatra
+- App will run on localhost:8080
+- From terminal, run the app:
+  * ruby lyft.rb
+- App runs on localhost:8080
+
+Requirements:
+    - Accept a POST request to the route “/test”, which accepts one argument “string_to_cut”.
+
+    - Return a JSON object with the key “return_string” and a string containing every third letter from the original string.
---
+++ lyft.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+require 'json'
+
+set :bind, '0.0.0.0'
+set :port, 8080
+
+def cut_string(str)
+  # declare the string we will return
+  return_string = ""
+  str.each_char.with_index do |c, i|
+    # push every third letter of input string into return_string
+    if i != 0 && ((i + 1) % 3 == 0)
+      return_string += c
+    end
+  end
+  return return_string
+end
+
+def app
+  get '/' do
+    erb :index
+  end
+
+  # Return a JSON object:{“return_string”: "cut_string"}
+  post '/test' do
+    cut_str = cut_string(params[:return_string])
+    str_to_parse = "{\"return_string\": \"#{cut_str}\"}"
+    hash = JSON.parse(str_to_parse)
+    "#{hash}"
+  end
+end
+
+app()
+
+
+
+
+
